48edf3dd8a478eb1b37cd3e4820b0e982d6aa8fd
Rafael <rreynoud@gmail.com>
2019-08-30 07:22:44 -0300

    Criando primeiro component POst

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
-   <h1>Oi eu sou o goku!</h1>
+
+   <h1>Vue Js</h1>
+
+   <Post />
+
   </div>
 </template>
+
+<script>
+
+import { Post } from './components/Post'
+
+export default {
+  components: {
+    Post
+  }
+}
+</script>
---
+++ src/components/Post.vue
@@ -0,0 +1,5 @@
+<template>
+    
+    <h2>Comnponents no vue</h2>
+    
+</template>
