f7a3742eb8172d6b60c87e7f0028e5ac7ca71845
Rafael <rreynoud@gmail.com>
2019-09-01 07:56:25 -0300

    Passando usando slot passando valores do component pai para o component filho

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <template>
-  <div id="app">
+  <div id="app" class="container" >
 
    <h1>Vue Js</h1>
 
-   <Post />
+  <Post>
+    <h2 slot="cabecalho" >Components no vue</h2>
+    
+    <p class="post-paragrafo" >Components são importantes no vue</p>
+    <span>...</span>
+  
+    <small slot="rodape" >por {{autor}} </small>    
+  </Post>
+
+
 
   </div>
 </template>
 
 <script>
 
-import { Post } from './components/Post'
+import  Post  from './components/Post'
 
 export default {
   components: {
     Post
+  },
+  data(){
+    return{
+      autor: 'Arduino'
+    }
   }
 }
 </script>
+
+<style scoped>
+  .container{
+    width: 960px;
+    margin: auto;
+  }
+
+ 
+</style>
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,5 +1,47 @@
 <template>
-    
-    <h2>Comnponents no vue</h2>
-    
+  <div class="post">
+    <div class="post-cabecalho">
+      <slot name="cabecalho" ></slot>
+    </div>
+
+    <div class="post-conteudo">
+      <slot ></slot>
+    </div>
+
+    <div class="post-rodape">
+      <slot name="rodape" ></slot>
+      <a href="#" class="link" >
+        <slot name="botao" >Ler mais..</slot>
+      </a>
+    </div>
+  </div>  
 </template>
+
+<style scoped>
+  
+  .post{
+    margin: 15px auto;
+  }
+
+  .post-cabecalho{
+    padding: 5px 12px;
+    background-color: #eee;
+  }
+
+  .post-conteudo{
+    font-size: 16px;
+  }
+
+  .post-rodape{
+    font-style: italic;
+  }
+
+  .link{
+    float: right;
+  }
+
+ .post-paragrafo{
+    color: red;
+  }
+
+</style>
